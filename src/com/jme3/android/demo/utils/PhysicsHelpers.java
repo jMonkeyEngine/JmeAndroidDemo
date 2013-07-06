e01c2ca432e05790cc9750e19a1912135c766cce
iwgeric <iwgeric@gmail.com>
2013-07-06 00:52:15 -0400

    Add PhysicsHelpers class to provide static methods for common physics routines
    Added a method to find the PhysicsControl on a spatial or parent.  Searches up the tree until a PhysicsControl is found.

---
+++ src/com/jme3/android/demo/utils/PhysicsHelpers.java
@@ -0,0 +1,30 @@
+package com.jme3.android.demo.utils;
+
+import com.jme3.bullet.control.PhysicsControl;
+import com.jme3.scene.Spatial;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author iwgeric
+ */
+public class PhysicsHelpers {
+    private static final Logger logger = Logger.getLogger(PhysicsHelpers.class.getName());
+
+    public static PhysicsControl getPhysicsControl(Spatial spatial) {
+        PhysicsControl physicsControl = spatial.getControl(PhysicsControl.class);
+
+        while (physicsControl == null) {
+            if (spatial.getParent() != null) {
+                spatial = spatial.getParent();
+                physicsControl = spatial.getControl(PhysicsControl.class);
+            } else {
+                break;
+            }
+        }
+
+        logger.log(Level.INFO, "PhysicsControl found on: {0}", spatial);
+        return physicsControl;
+    }
+}
