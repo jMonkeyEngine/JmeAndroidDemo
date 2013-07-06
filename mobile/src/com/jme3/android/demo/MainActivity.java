e16508287adc1c453d7d10842003ee698dbc4a22
iwgeric <iwgeric@gmail.com>
2013-07-06 10:54:57 -0400

    Load correct Audio Renderer based on Android version
    If Android 2.2 is being used, MediaPlayer/SoundPool Audio is used.  If Android 2.3 or higher, OpenAL Soft is used.

--- mobile/src/com/jme3/android/demo/MainActivity.java
+++ mobile/src/com/jme3/android/demo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jme3.android.demo;
 
 import android.content.pm.ActivityInfo;
+import android.os.Build;
 import com.jme3.app.AndroidHarness;
+import com.jme3.system.AppSettings;
 import com.jme3.system.android.AndroidConfigChooser.ConfigType;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -31,8 +33,20 @@ public class MainActivity extends AndroidHarness{
         mouseEventsEnabled = true;
         // Set the default logging level (default=Level.INFO, Level.ALL=All Debug Info)
         LogManager.getLogManager().getLogger("").setLevel(Level.INFO);
-        
+
         splashPicID = R.drawable.monkey256_9;
+
+        // NOTE: SDK_INT is only available since 1.6,
+        // but for jME3 it doesn't matter since android versions 1.5 and below
+        // are not supported.
+        if (Build.VERSION.SDK_INT >= 9){
+            logger.log(Level.INFO, "Enabling OpenAL Soft Audio (Android 2.3 or higher)");
+            audioRendererType = AppSettings.ANDROID_OPENAL_SOFT;
+        } else {
+            logger.log(Level.INFO, "Enabling Android MediaPlayer/SoundPool Audio (Android 2.2)");
+            audioRendererType = AppSettings.ANDROID_MEDIAPLAYER;
+        }
+
     }
 
 }
